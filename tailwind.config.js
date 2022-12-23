a43a2db6255099b1abb19ed9ecd4a59bdd49da45
RakiKadamR <121303323+RakiKadamR@users.noreply.github.com>
2022-12-23 23:20:36 +0530

    2 commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+module.exports = {
+  content: [
+    "./components/**/*.js",
+    "./pages/**/*.{ts,tsx,js}",
+    "./public/**/*.html",
+  ],
+  plugins: [],
+  theme: {},
+};
